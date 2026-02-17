520e2b15281f3f4149483eaa12be3b69d4bd61b6
haru-094 <a.nasir@alustudent.com>
2026-02-17 10:09:09 +0300

    add edit and delete functionaltly

--- scripts/state.js
+++ scripts/state.js
@@ -4,7 +4,7 @@ export function new_transaction(data) {
   let record_new = {
     id: `record_${Date.now()}`,
     description: data.name,
-    amount: data.amount,
+    amount: parseFloat(data.amount),
     category: data.category,
     date: data.date,
     createdAt: new Date().toISOString(),
@@ -14,6 +14,26 @@ export function new_transaction(data) {
   transaction.push(record_new);
   localStorage.setItem("finance_data_local", JSON.stringify(transaction));
   return record_new;
+}
+
+export function delete_transaction(id) {
+  transaction = transaction.filter((item) => item.id !== id);
+  localStorage.setItem("finance_data_local", JSON.stringify(transaction));
+}
+
+export function update_transaction(id, updatedData) {
+  const index = transaction.findIndex((t) => t.id === id);
+  if (index !== -1) {
+    transaction[index] = {
+      ...transaction[index],
+      description: updatedData.name,
+      amount: parseFloat(updatedData.amount),
+      date: updatedData.date,
+      category: updatedData.category,
+      updatedAt: new Date().toISOString(),
+    };
+    localStorage.setItem("finance_data_local", JSON.stringify(transaction));
+  }
 }
 
 export function get_transaction() {
